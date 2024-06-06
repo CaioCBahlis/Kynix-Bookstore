574433f6075b447f3bedaec349607af710b4d79a
CaioCBahlis <104174800+CaioCBahlis@users.noreply.github.com>
2024-06-06 17:22:35 -0300

    Add files via upload

--- GOTH_STACK/goth_main.go
+++ GOTH_STACK/goth_main.go
@@ -41,25 +41,60 @@ func handler(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
+/*
+func SearchHandler(w http.ResponseWriter, r *http.Request) {
+
+	if r.Method == http.MethodPost {
+		r.ParseForm()
+		searchQuery := r.FormValue("searchtext")
+
+		// Now you can use the searchQuery to fetch data from the database or perform any other logic.
+		db := MyDatabase.OpenConn()
+		Generate_Row(db, searchQuery, "Search Results")
+
+		tmplPath := filepath.Join("C:/Users/User/Desktop/Code/GOTH_STACK/templates", "SearchPage.html")
+		tmpl, err := template.ParseFiles(tmplPath)
+		if err != nil {
+			http.Error(w, "Error parsing template: "+err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		err = tmpl.Execute(w, MyData)
+		if err != nil {
+			http.Error(w, "Error executing template: "+err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+	} else {
+		http.Error(w, "Invalid request method", http.StatusMethodNotAllowed)
+	}
+}
+*/
+
+
 
 func main() {
+	
+	db := MyDatabase.OpenConn()
 
 	MyData = Data{
 		Sections: []Section{},
 	}
 
-	db := MyDatabase.OpenConn()
-	Generate_Row(db, "esgrima", "Birds Of A Feather")
-	Generate_Row(db, "esgrima", "Mitski")
-	Generate_Row(db, "esgrima", "Compiladores")
-
+	Generate_Row(db, "esgrima", "Secao 1")
+	Generate_Row(db, "livro", "secao 2")
+	Generate_Row(db, "esgrima", "secao 3")
+	Generate_Row(db, "livro", "secao 4")
 	
 	
 
 	http.HandleFunc("/", handler)
+	//http.HandleFunc("/SearchPage", SearchHandler)
+
 	staticPath := "C:/Users/User/Desktop/Code/GOTH_STACK/static"
 	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir(staticPath))))
-	http.ListenAndServe(":8102", nil)
+
+	http.ListenAndServe(":8103", nil)
 }
 
 
@@ -73,8 +108,10 @@ func Generate_Row(db *sql.DB, SearchProduct string, RowTitle string){
 		Products: []MyDatabase.Product{},
 	}
 	
-	
-	for rows.Next(){
+	ElementsPerRow := 6
+
+	for i := 0; i < ElementsPerRow; i++{
+		rows.Next()
 		var p MyDatabase.Product
 		err := rows.Scan(&p.Title, &p.Price, &p.Reviews, &p.Imgurl, &p.Purl, &p.Lupdate, &p.Seller)
 		if err != nil {
